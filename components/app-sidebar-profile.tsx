78de6182e66af684604835281f6e967a04dc7511
Davide Tarulli <davidetarulli.dev@gmail.com>
2025-02-08 16:55:26 +0100

    Aggiornato il componente AppSidebar con nuove funzionalità, inclusa la visualizzazione del nome utente e un menu a discesa per il logout. Modificati i link e migliorata la struttura del sidebar.

--- components/app-sidebar-profile.tsx
+++ components/app-sidebar-profile.tsx
@@ -10,20 +10,24 @@ import {
     SidebarMenuButton,
     SidebarMenuItem,
   } from "@/components/ui/sidebar"
-import { BedDouble, Calendar, Home, UserPen } from "lucide-react"
+import { BedDouble, Calendar, ChevronLeft, ChevronUp, Home, LogOut, User2, UserPen, UserRoundCog } from "lucide-react"
+import Link from "next/link"
+import { Button } from "./ui/button"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "./ui/dropdown-menu"
+import { SignOutButton } from "@clerk/nextjs"
 
 
+interface AppSidebarProps {
+  accountName: {
+    nome: string | undefined,
+    cognome: string | undefined
+  }
+}
 
 
-
-export function AppSidebar() {
+export function AppSidebar({accountName}: AppSidebarProps) {
   
   const items = [
-    {
-      title: "Torna alla Home",
-      url: "/",
-      icon: Home,
-    },
     {
       title: "Il mio Profilo",
       url: "/account/myprofile",
@@ -31,7 +35,7 @@ export function AppSidebar() {
     },
     {
       title: "Le mie Prenotazioni",
-      url: "/account/bookings",
+      url: "/account/mybookings",
       icon: Calendar,
     },
   ]
@@ -39,6 +43,14 @@ export function AppSidebar() {
     return (
       <Sidebar variant="inset" collapsible="none" className="h-screen bg-white">
         <SidebarHeader>
+            <div>
+              <Button variant={"ghost"} className="pl-0.5">
+                <Link href="/" className="flex items-center gap-2">
+                  <ChevronLeft />
+                  Torna alla Home
+                </Link>
+              </Button>
+            </div>
             <div className="flex gap-2 justify-center items-center py-5">
                 <div className="flex size-6 bg-primarflex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
                     <BedDouble className="size-4" />
@@ -48,16 +60,16 @@ export function AppSidebar() {
         </SidebarHeader>
         <SidebarContent>
           <SidebarGroup>
-          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupLabel>Account</SidebarGroupLabel>
           <SidebarGroupContent>
             <SidebarMenu>
               {items.map((item) => (
                 <SidebarMenuItem key={item.title}>
                   <SidebarMenuButton asChild>
-                    <a href={item.url}>
+                    <Link href={item.url}>
                       <item.icon />
                       <span>{item.title}</span>
-                    </a>
+                    </Link>
                   </SidebarMenuButton>
                 </SidebarMenuItem>
               ))}
@@ -65,7 +77,31 @@ export function AppSidebar() {
           </SidebarGroupContent>  
           </SidebarGroup>
         </SidebarContent>
-        <SidebarFooter />
+        <SidebarFooter>
+            <SidebarMenu>
+                <SidebarMenuItem>
+                <DropdownMenu>
+                    <DropdownMenuTrigger className="group" asChild>
+                      <SidebarMenuButton>
+                          <User2 /> {accountName?.nome + " " + accountName?.cognome}
+                          <ChevronUp className="ml-auto transition-transform group-data-[state=open]:rotate-180" />
+                      </SidebarMenuButton>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent
+                    side="top"
+                    className="w-[--radix-popper-anchor-width]"
+                    >
+                    <SignOutButton redirectUrl="/">
+                      <DropdownMenuItem>
+                        <LogOut />
+                        Logout
+                      </DropdownMenuItem>
+                    </SignOutButton>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+                </SidebarMenuItem>
+            </SidebarMenu>
+        </SidebarFooter>
       </Sidebar>
     )
   }
