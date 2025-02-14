442493b234ea46bbe013708611026efe7c907982
Davide Tarulli <davidetarulli.dev@gmail.com>
2025-02-14 18:30:34 +0100

    Aggiunta logica di reindirizzamento per il ruolo GOVERNANTE nella pagina admin

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -2,13 +2,21 @@ import { SidebarInset, SidebarProvider, SidebarTrigger } from "@/components/ui/s
 import { AppSidebar } from "@/components/app-sidebar"
 import getUser from "../lib/user"
 import { ruolo } from "@prisma/client"
-import { redirect } from "next/navigation"
+import {  redirect } from "next/navigation";
+import { headers } from "next/headers";
  
 export default async function Layout({ children }: { children: React.ReactNode }) {
   const user = await getUser()
 
   if (user?.ruolo !== ruolo.PROPRIETARIO && user?.ruolo !== ruolo.GOVERNANTE) {
     return redirect("/")
+  }
+
+  if(user?.ruolo === ruolo.GOVERNANTE) {
+      const currentUrl = new URL((await headers()).get("x-nextjs-url") ?? "", process.env.NEXT_PUBLIC_APP_URL);
+      if (!currentUrl.pathname.startsWith("/admin/pulizie")) {
+        return redirect("/admin/pulizie");
+      }
   }
 
   const accountName = {
